7fec53496c8bbe6dc288be9586bbf689765d7a0f
kk36931112 <kk36931112@gmail.com>
2024-08-07 16:45:37 +0900

    Add files via upload

---
+++ day02/ForExam1.java
@@ -0,0 +1,24 @@
+package day02;
+
+public class ForExam1 {
+	
+	public static void main(String[] args) {
+		// for(초기식; 조기식; 증감식) { 반복 순행문 }
+		int i;
+		for (i = 2; i<=9; i++) {
+			System.out.println("##"+i+"단##");
+			
+		for(int j = 1; j<=9; j++) {
+			System.out.println(i+"* "+ j+"=" +(i*j));
+		}
+	
+		}
+		
+		
+
+	
+	
+	
+}	
+
+}
